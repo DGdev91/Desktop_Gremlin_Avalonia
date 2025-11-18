e50673a2cd6b540dc14e4f9563b065cb405c753c
Davide Guidotti <DGdev91@users.noreply.github.com>
2025-11-18 14:34:22 +0100

    Rename Sprites directory to SpriteSheet in build script

--- buildlinux.sh
+++ buildlinux.sh
@@ -1,5 +1,5 @@
 #Thanks to Cautami (https://github.com/Cautami) for this script, taken from https://github.com/Cautami/Desktop_Gremlin_Avalonia
 dotnet publish Desktop_Gremlin/Desktop_Gremlin.csproj -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true -o Desktop_Gremlin/bin/Published/Linux
-cp -r Desktop_Gremlin/Sprites Desktop_Gremlin/bin/Published/Linux/
+cp -r Desktop_Gremlin/SpriteSheet Desktop_Gremlin/bin/Published/Linux/
 cp Desktop_Gremlin/ico.ico Desktop_Gremlin/bin/Published/Linux/
 cp Desktop_Gremlin/config.txt Desktop_Gremlin/bin/Published/Linux/
